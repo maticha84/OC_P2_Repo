0f6aa73bdeb884cea53841794d2bd898a48e700e
maticha84 <maticha84@gmail.com>
2021-05-06 14:52:10 +0200

    Start dev for one book and requirements.txt code OneBook.py : - title - product_description

--- OC_P2_Project/Modules/OneBook.py
+++ OC_P2_Project/Modules/OneBook.py
@@ -1,3 +1,41 @@
 """
 Ici, on va créer les fonctions permettant de scrapper une seule page
 """
+
+import requests
+from bs4 import BeautifulSoup
+import re
+
+url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
+
+response = requests.get(url)
+
+if response.ok:
+    soup = BeautifulSoup(response.text, "html.parser")
+    """
+    Title
+    """
+    title = soup.find('title')
+    """
+    Product description
+    """
+    articles = soup.findAll('article')
+    tests = []
+    for article in articles:
+        p=article.find('p',attrs = None)
+        if p != None:
+            #print(p.text)
+            product_description = p.text
+
+    print(product_description)
+    """
+    Product Information
+    - UPC
+    - Price excluding tax
+    - Price Including tax
+    - number available
+    - review rating
+    """
+
+else:
+    print('error')
