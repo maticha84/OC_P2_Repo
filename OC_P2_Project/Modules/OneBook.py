4c0ab832a5798aa06d8ef641c28fd9f2e10648fb
maticha84 <maticha84@gmail.com>
2021-05-06 16:04:33 +0200

    Dev for one book code OneBook.py : - product information

--- OC_P2_Project/Modules/OneBook.py
+++ OC_P2_Project/Modules/OneBook.py
@@ -40,6 +40,43 @@ if response.ok:
     - number available
     - review rating
     """
+    trs = soup.findAll('tr')
+    for tr in trs:
+        th = tr.find('th')
+        #print(th.text)
+        td = tr.find('td')
+        #print(td.text)
+        if th.text == "UPC":
+            universal_product_code = td.text
+            #break
+        elif th.text == "Price (excl. tax)":
+            price_excluding_tax = td.text.split('Â')[1]
+            #break
+        elif th.text == "Price (incl. tax)":
+            price_including_tax = td.text.split('Â')[1]
+            #break
+        elif th.text == "Availability":
+            number_available = td.text
+            #break
+        elif th.text == "Number of reviews":
+            review_rating = td.text
+            #break
 
+    """print(universal_product_code+'\n'+price_excluding_tax + '\n'+price_including_tax +
+          '\n' + number_available + '\n'+ review_rating)
+    """
+    """
+    Category
+    <a href="../category/books/fantasy_19/index.html">Fantasy</a>
+    """
+    """
+    Image_url
+    <img src="../../media/cache/8e/5a/8e5a6639c7e2f9b59ff15f04a3b055e1.jpg" alt="Eragon (The Inheritance Cycle #1)">
+    """
+    """
+    Product_page_url
+    """
+    product_page_url = url
+    #print(product_page_url)
 else:
     print('error')
