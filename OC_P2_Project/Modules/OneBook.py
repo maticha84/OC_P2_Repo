281780a122db9f571c3053fe12e26ba0670c838c
maticha84 <maticha84@gmail.com>
2021-05-07 16:11:25 +0200

    Dev for one book code OneBook.py : - def rech_info_page(soup)

--- OC_P2_Project/Modules/OneBook.py
+++ OC_P2_Project/Modules/OneBook.py
@@ -6,32 +6,26 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
 
-response = requests.get(url)
-
-if response.ok:
-    soup = BeautifulSoup(response.text, "html.parser")
+def rech_info_page(soup):
     """
     Title
     """
     titles = soup.find('title')
     title = titles.text.split('|')[0]
     title = title.strip()
-    #print(title)
+    # print(title)
 
     """
     Product description
     """
     articles = soup.findAll('article')
-    tests = []
     for article in articles:
-        p=article.find('p',attrs = None)
+        p = article.find('p', attrs=None)
         if p != None:
-            #print(p.text)
             product_description = p.text
 
-    #print(product_description)
+    # print(product_description)
     """
     Product Information
     - UPC
@@ -43,28 +37,25 @@ if response.ok:
     trs = soup.findAll('tr')
     for tr in trs:
         th = tr.find('th')
-        #print(th.text)
+        # print(th.text)
         td = tr.find('td')
-        #print(td.text)
+        # print(td.text)
         if th.text == "UPC":
             universal_product_code = td.text
-            #break
+            # break
         elif th.text == "Price (excl. tax)":
             price_excluding_tax = td.text.split('Â')[1]
-            #break
+            # break
         elif th.text == "Price (incl. tax)":
             price_including_tax = td.text.split('Â')[1]
-            #break
+            # break
         elif th.text == "Availability":
             number_available = td.text
-            #break
+            # break
         elif th.text == "Number of reviews":
             review_rating = td.text
-            #break
+            # break
 
-    """print(universal_product_code+'\n'+price_excluding_tax + '\n'+price_including_tax +
-          '\n' + number_available + '\n'+ review_rating)
-    """
     """
     Category
     <a href="../category/books/fantasy_19/index.html">Fantasy</a>
@@ -77,6 +68,24 @@ if response.ok:
     Product_page_url
     """
     product_page_url = url
-    #print(product_page_url)
-else:
-    print('error')
+    return title, product_description, universal_product_code, price_excluding_tax, price_including_tax, \
+           number_available, review_rating
+    # print(product_page_url)
+
+
+if __name__ == '__main__':
+    url = "http://books.toscrape.com/catalogue/eragon-the-inheritance-cycle-1_153/index.html"
+
+    response = requests.get(url)
+
+    if response.ok:
+        soup = BeautifulSoup(response.text, "html.parser")
+        title, product_description, universal_product_code, price_excluding_tax, price_including_tax, number_available,\
+        review_rating = rech_info_page(soup)
+        print(title)
+        print(product_description)
+        print(universal_product_code)
+        print(price_excluding_tax)
+        print(price_including_tax)
+        print(number_available)
+        print(review_rating)
