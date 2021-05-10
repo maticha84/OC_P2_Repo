49eb7ff142cdedbb23af325d9232ce0b1c14b2aa
maticha84 <maticha84@gmail.com>
2021-05-10 09:26:33 +0200

    Dev for one category - AllBooksByCategory.py code OneCategory.py: modification fonction - rech_tab_category
    code AllBooksByCatergory.py:
    creation fichier

---
+++ OC_P2_Project/Modules/AllBooksByCatergory.py
@@ -0,0 +1,4 @@
+"""
+Ici ce trouveront les fonction permettant de rechercher les différentes url de chaque
+catégory, pour faire la recherce par catégorie après.
+"""
--- OC_P2_Project/Modules/OneCategory.py
+++ OC_P2_Project/Modules/OneCategory.py
@@ -11,39 +11,72 @@ import urllib.request
 from bs4 import BeautifulSoup
 import re
 
-def rech_info_category(url_cat):
-    response = requests.get(url_cat)
+def rech_info_category(soup):
 
-    if response.ok:
-        soup = BeautifulSoup(response.text, "html.parser")
 
-        ' Recherche du nom de la catégorie'
-        ul = soup.find('ul',attrs='breadcrumb',)
-        li = ul.find('li',attrs='active')
-        category = li.text
+    #Recherche du nom de la catégorie
+    ul = soup.find('ul',attrs='breadcrumb',)
+    li = ul.find('li',attrs='active')
+    category = li.text
     return category
     pass
 
-def rech_tab_category(url_cat,category):
+def rech_tab_category(soup,category):
 
     list_books=[]
 
-    list_books.append('test')
+    # choix : il n'y a qu'une page ou il y en a plusieurs
+
+
+    # si plusieurs pages, alors
+    if soup.find('ul',attrs = "pager") :
+        # récupérer le nombre x de pages à scroller,
+        pager = soup.find('ul',attrs = "pager").text
+        pager = pager.strip()
+        pager = pager.split()
+        x = int(pager[3])
+        print(x)
+        # pour url_page1  à url_pagex
+        for i in range(1,x+1,1):
+            print(i)
+            url_page = url_cat.split('/index.html')
+            urlpage = url_page[0]+'/page-'+str(i)+'.html'
+            print(urlpage)
+            # recherche des liens dans les pages et les ajouter dans le tableau des liens
+            rech_liens = requests.get(urlpage)
+            if rech_liens.ok:
+                soup_liens = BeautifulSoup(rech_liens.text, "html.parser")
+                h3 = soup_liens.findAll('h3')
+                print(h3)
+
+            pass
+
+
+    # si une seule page, alors
+    else:
+        print('KO')
+    # recherche des liens dans la pageet les ajouter dans le tableau des liens
     list_books.append('test2')
 
 
 
 
 
+    #retourne la liste des url des livres de la catégorie
     return list_books
 
 
 if __name__ == '__main__':
     url_cat = 'http://books.toscrape.com/catalogue/category/books/mystery_3/index.html'
-    category = rech_info_category(url_cat)
+    #url_cat = 'http://books.toscrape.com/catalogue/category/books/travel_2/index.html'
+    response = requests.get(url_cat)
 
-    list_books = rech_tab_category(url_cat,category)
-    print(list_books)
+    if response.ok:
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        category = rech_info_category(soup)
+        list_books = rech_tab_category(soup,category)
+        print(list_books)
 
     #print(rech_info_category(url_cat))
 
