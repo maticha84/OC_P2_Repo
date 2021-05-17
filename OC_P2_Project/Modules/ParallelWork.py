97db0f5ecb42fbdcff2233b33aac5ddb32d0869a
maticha84 <maticha84@gmail.com>
2021-05-17 10:54:06 +0200

    Mise à jour README.md Prise en compte du Thread dans l'explication du README.md

--- OC_P2_Project/Modules/ParallelWork.py
+++ OC_P2_Project/Modules/ParallelWork.py
@@ -7,7 +7,11 @@ from Modules import OneCategory as oc
 
 
 class ParallelWorkGlobal(Thread):
-
+    """
+    Classe permettant la mise en place de parallèlisation entre les exécutions des différentes catégories.
+    queue : les urls à scapper, qui sont mises en file d'attente
+    urlsite : l'adresse url du site : "http://books.toscrape.com"
+    """
 
     def __init__(self, queue,urlsite):
         Thread.__init__(self)
@@ -21,15 +25,3 @@ class ParallelWorkGlobal(Thread):
             abc.result_main(self.urlsite,urlCat)
             self.queue.task_done()
 
-
-class ParrallelImageByCategory(Thread):
-    def __init__ (self, queue, urlsite,dico):
-        Thread.__init__(self)
-        self.queue = queue
-        self.urlsite = urlsite
-        self.dico = dico
-
-    def run(self):
-        book = self.queue.get()
-        oc.dico_for_csv(book, self.urlsite, self.dico)
-        self.queue.task_done()
